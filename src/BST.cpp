c16a4d00943d03fb834da2a791ddf3e5217adc4c
japopich <japopich@hotmail.com>
2020-04-18 16:31:20 -0600

    Added files needed for project

---
+++ src/BST.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file BST.cpp
+ * @brief The main source file for the BST Class
+ */
+
+#include "BST.hpp"
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "hash.hpp"
+#include "LinkedList.hpp"
+#include "BST.hpp"
+
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
