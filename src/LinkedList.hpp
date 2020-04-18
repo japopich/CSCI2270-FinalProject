b9e2c004c12089729a92f90fc70543527167e54b
matthewj <matthewj0717@gmail.com>
2020-04-18 17:55:49 -0600

    Implemented Linked List

--- src/LinkedList.hpp
+++ src/LinkedList.hpp
@@ -5,6 +5,25 @@
 #ifndef _LINKED_LIST_HPP_
 #define _LINKED_LIST_HPP_
 
+struct Node
+{
+    int key;
+    Node *next;
+};
 
+class LinkedList
+{
+    private:
+        Node *head;
+
+    public:
+        LinkedList();
+        ~LinkedList();
+        void insert(Node *prev, int newKey); // Inserts after prev
+        void insert(int newKey); // Inserts at the end of the list
+        Node *search(int key);
+        void displayAll(); // Displays all nodes in the list
+        void displayFirst100(); // Displays the first 100 nodes in the list
+};
 
 #endif
