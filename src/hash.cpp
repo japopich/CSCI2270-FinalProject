535c8bf68288c10c4637b189d30eab35bae4b7da
matthewj <matthewj0717@gmail.com>
2020-04-22 18:47:13 -0600

    Implemented hash constructor and destructor

--- src/hash.cpp
+++ src/hash.cpp
@@ -5,19 +5,41 @@
 
 #include "hash.hpp"
 
-HashTable::HashTable(int bsize)
+HashTable::HashTable(int size)
 {
-
+    // Declare the dynamic array of node pointers and set tableSize and numOfcollision to 0
+    table = new node *[size];
+    tableSize = size;
+    numOfcollision = 0;
 }
 
 HashTable::~HashTable()
 {
-    
+    // Iterate through the array and delete each node object stored in it, then delete the array
+    for (int i = 0; i < tableSize; i++)
+    {
+        // If a node is stored in the ith index of the array delete it
+        if (table[i] != nullptr)
+        {
+            // Iterate through the nodes stored at i and delete them
+            node *curr = table[i];
+            node *prev = nullptr;
+            while (curr != nullptr)
+            {
+                prev = curr;
+                curr = curr->next;
+                delete prev;
+            }
+        }
+    }
+
+    // Delete the dynamic array
+    delete table;
 }
 
-node* HashTable::createNode(int key, node* next)
+node *HashTable::createNode(int key, node *next)
 {
-    node* newNode = new node;
+    node *newNode = new node;
     newNode->key = key;
     newNode->next = next;
     return newNode;
@@ -31,10 +53,10 @@ unsigned int HashTable::hashFunction(int key)
 bool HashTable::insertLLitem(int key)
 {
     int nodeIndex = HashTable::hashFunction(key);
-    node* newNode = HashTable::createNode(key, nullptr);
+    node *newNode = HashTable::createNode(key, nullptr);
 
     // If no node exists at that index append newNode as head
-    if(HashTable::table[nodeIndex] == nullptr)
+    if (HashTable::table[nodeIndex] == nullptr)
     {
         newNode->prev = nullptr;
         HashTable::table[nodeIndex] = newNode;
@@ -44,12 +66,12 @@ bool HashTable::insertLLitem(int key)
     {
         HashTable::numOfcollision++; // Add to collision....
         // Set the node at the Hash Table index as our LinkedList (LL) head
-        node* head = HashTable::table[nodeIndex];
+        node *head = HashTable::table[nodeIndex];
 
         // Check for duplicates in LL
-        while(head != nullptr)
+        while (head != nullptr)
         {
-            if(head->key == key)
+            if (head->key == key)
             {
                 std::cout << "Duplicate Key!" << std::endl;
                 return false;
@@ -59,7 +81,7 @@ bool HashTable::insertLLitem(int key)
 
         // NO duplicates so append the new node
         head = HashTable::table[nodeIndex]; // Set Head again
-        while((head != nullptr) && (newNode->key > head->key))
+        while ((head != nullptr) && (newNode->key > head->key))
         {
             head = head->next;
         }
@@ -75,33 +97,32 @@ bool HashTable::insertLLitem(int key)
 
 bool HashTable::insertLinearitem(int key)
 {
-
 }
 
 bool HashTable::insertQuaditem(int key)
 {
-
 }
 
-node* HashTable::searchLLItem(int key)
+node *HashTable::searchLLItem(int key)
 {
     // Get the index of where the key should be
     int nodeIndex = HashTable::hashFunction(key);
 
     // If an index does exist for that key enter if statement
-    if(HashTable::table[nodeIndex] != nullptr)
+    if (HashTable::table[nodeIndex] != nullptr)
     {
         // Set the currNode to the node at the index
-        node* currNode = HashTable::table[nodeIndex];
+        node *currNode = HashTable::table[nodeIndex];
 
         // If the node is not equal to nullptr, check to see if key is equal to key we're looking for
         // if not check attached LinkedList
-        while(currNode != nullptr)
+        while (currNode != nullptr)
         {
-            if(currNode->key == key)
+            if (currNode->key == key)
             {
                 return currNode;
-            }else
+            }
+            else
             {
                 currNode = currNode->next;
             }
@@ -110,19 +131,16 @@ node* HashTable::searchLLItem(int key)
     return nullptr; // If key not found at all return nullptr
 }
 
-node* HashTable::searchLinearItem(int key)
+node *HashTable::searchLinearItem(int key)
 {
-
 }
 
-node* HashTable::searchQuadItem(int key)
+node *HashTable::searchQuadItem(int key)
 {
-    
 }
 
 void HashTable::printTable()
 {
-
 }
 
 int HashTable::getNumOfCollision()
