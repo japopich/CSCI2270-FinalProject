5799417528bac44d740023373bb438711d2284a1
japopich <japopich@hotmail.com>
2020-04-24 20:23:55 -0600

    Updated the MATLAB script for collisions

--- MATLAB/PlotResults.m
+++ MATLAB/PlotResults.m
@@ -29,21 +29,39 @@ LL_out_2 = table2array(LL_out_2);
 
 LinHash_out_1 = readtable('../bin/LinHash_out_1.csv');
 LinHash_out_1 = table2array(LinHash_out_1);
+LinHash_Collision_1 = LinHash_out_1(:,3);
+LinHash_Search_1 = LinHash_out_1(:,3);
+LinHash_out_1 = LinHash_out_1(:,[1 2]);
 
 LinHash_out_2 = readtable('../bin/LinHash_out_2.csv');
 LinHash_out_2 = table2array(LinHash_out_2);
+LinHash_Collision_2 = LinHash_out_2(:,3);
+LinHash_Search_2 = LinHash_out_2(:,3);
+LinHash_out_2 = LinHash_out_2(:,[1 2]);
 
 LLHash_out_1 = readtable('../bin/LLHash_out_1.csv');
 LLHash_out_1 = table2array(LLHash_out_1);
+LLHash_Collision_1 = LLHash_out_1(:,3);
+LLHash_Search_1 = LLHash_out_1(:,3);
+LLHash_out_1 = LLHash_out_1(:,[1 2]);
 
 LLHash_out_2 = readtable('../bin/LLHash_out_2.csv');
 LLHash_out_2 = table2array(LLHash_out_2);
+LLHash_Collision_2 = LLHash_out_2(:,3);
+LLHash_Search_2 = LLHash_out_2(:,3);
+LLHash_out_2 = LLHash_out_2(:,[1 2]);
 
 QuadHash_out_1 = readtable('../bin/QuadHash_out_1.csv');
 QuadHash_out_1 = table2array(QuadHash_out_1);
+QuadHash_Collision_1 = QuadHash_out_1(:,3);
+QuadHash_Search_1 = QuadHash_out_1(:,3);
+QuadHash_out_1 = QuadHash_out_1(:,[1 2]);
 
 QuadHash_out_2 = readtable('../bin/QuadHash_out_2.csv');
 QuadHash_out_2 = table2array(QuadHash_out_2);
+QuadHash_Collision_2 = QuadHash_out_2(:,3);
+QuadHash_Search_2 = QuadHash_out_2(:,3);
+QuadHash_out_2 = QuadHash_out_2(:,[1 2]);
 
 %% PLOT BST
 f1 = figure;
@@ -86,18 +104,33 @@ print(f6, '-depsc', 'LL_out_B');
 %% PLOT Linear Probing Hash table
 f3 = figure;
 figure(f3);
-plot(LinHash_out_1, 'LineWidth',1.5);
+yyaxis left
+hold on
+plot(LinHash_out_1(:,1), 'LineWidth',1.5);
+plot(LinHash_out_1(:,2), 'r-', 'LineWidth',1.5);
+
+yyaxis right
+plot(LinHash_Collision_1, 'k*', 'LineWidth', 1.0);
+plot(LinHash_Search_1, 'm.');
 xlabel('Number of Elements in Data Structure (10^2)');
-legend('Insert Avg Time', 'Search Avg Time', 'Location', 'northwest');
+legend('Insert Avg Time', 'Search Avg Time', 'Insert Collisions', 'Search Collisions', 'Location', 'northwest');
 
 % Output the plot to EPS for LATEX
 print(f3, '-depsc', 'LinHash_out_A');
 
 f4 = figure;
 figure(f4);
-plot(LinHash_out_2, 'LineWidth',1.5);
+yyaxis left
+hold on
+plot(LinHash_out_2(:,1), 'LineWidth',1.5);
+plot(LinHash_out_2(:,2), 'r-', 'LineWidth',1.5);
+
+yyaxis right
+plot(LinHash_Collision_2, 'k*', 'LineWidth', 1.0);
+plot(LinHash_Search_2, 'm.');
 xlabel('Number of Elements in Data Structure (10^2)');
-legend('Insert Avg Time', 'Search Avg Time', 'Location', 'northwest');
+legend('Insert Avg Time', 'Search Avg Time', 'Insert Collisions', 'Search Collisions', 'Location', 'northwest');
+
 
 % Output the plot to EPS for LATEX
 print(f4, '-depsc', 'LinHash_out_B');
@@ -105,18 +138,34 @@ print(f4, '-depsc', 'LinHash_out_B');
 %% PLOT Chaining Hash Table
 f7 = figure;
 figure(f7);
-plot(LLHash_out_1, 'LineWidth',1.5);
+yyaxis left
+hold on
+plot(LLHash_out_1(:,1), 'LineWidth',1.5);
+plot(LLHash_out_1(:,2), 'r-', 'LineWidth',1.5);
+
+yyaxis right
+plot(LLHash_Collision_1, 'k*', 'LineWidth', 1.0);
+plot(LLHash_Search_1, 'm.');
 xlabel('Number of Elements in Data Structure (10^2)');
-legend('Insert Avg Time', 'Search Avg Time', 'Location', 'northwest');
+legend('Insert Avg Time', 'Search Avg Time', 'Insert Collisions', 'Search Collisions', 'Location', 'northwest');
+
 
 % Output the plot to EPS for LATEX
 print(f7, '-depsc', 'LLHash_out_A');
 
 f8 = figure;
 figure(f8);
-plot(LLHash_out_2, 'LineWidth',1.5);
+yyaxis left
+hold on
+plot(LLHash_out_2(:,1), 'LineWidth',1.5);
+plot(LLHash_out_2(:,2), 'r-', 'LineWidth',1.5);
+
+yyaxis right
+plot(LLHash_Collision_2, 'k*', 'LineWidth', 1.0);
+plot(LLHash_Search_2, 'm.');
 xlabel('Number of Elements in Data Structure (10^2)');
-legend('Insert Avg Time', 'Search Avg Time', 'Location', 'northwest');
+legend('Insert Avg Time', 'Search Avg Time', 'Insert Collisions', 'Search Collisions', 'Location', 'northwest');
+
 
 % Output the plot to EPS for LATEX
 print(f8, '-depsc', 'LLHash_out_B');
@@ -124,18 +173,34 @@ print(f8, '-depsc', 'LLHash_out_B');
 %% PLOT Quadratic Probing Hash Table
 f9 = figure;
 figure(f9);
-plot(QuadHash_out_1, 'LineWidth',1.5);
+yyaxis left
+hold on
+plot(QuadHash_out_1(:,1), 'LineWidth',1.5);
+plot(QuadHash_out_1(:,2), 'r-', 'LineWidth',1.5);
+
+yyaxis right
+plot(QuadHash_Collision_1, 'k*', 'LineWidth', 1.0);
+plot(QuadHash_Search_1, 'm.');
 xlabel('Number of Elements in Data Structure (10^2)');
-legend('Insert Avg Time', 'Search Avg Time', 'Location', 'northwest');
+legend('Insert Avg Time', 'Search Avg Time', 'Insert Collisions', 'Search Collisions', 'Location', 'northwest');
+
 
 % Output the plot to EPS for LATEX
 print(f9, '-depsc', 'QuadHash_out_A');
 
 f10 = figure;
 figure(f10);
-plot(QuadHash_out_2, 'LineWidth',1.5);
+yyaxis left
+hold on
+plot(QuadHash_out_2(:,1), 'LineWidth',1.5);
+plot(QuadHash_out_2(:,2), 'r-', 'LineWidth',1.5);
+
+yyaxis right
+plot(QuadHash_Collision_2, 'k*', 'LineWidth', 1.0);
+plot(QuadHash_Search_2, 'm.');
 xlabel('Number of Elements in Data Structure (10^2)');
-legend('Insert Avg Time', 'Search Avg Time', 'Location', 'northwest');
+legend('Insert Avg Time', 'Search Avg Time', 'Insert Collisions', 'Search Collisions', 'Location', 'northwest');
+
 
 % Output the plot to EPS for LATEX
 print(f10, '-depsc', 'QuadHash_out_B');
